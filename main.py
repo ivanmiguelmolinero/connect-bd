a1bfeeebd792e851e8aacf653e5db52510525cbb
ivanmiguelmolinero <i.miguel.molinero@gmail.com>
2022-04-01 13:53:09 +0200

    Leo DB, parseo a JSON y subo a AWS S3

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 import pymysql
+import json
+import boto3
+from botocore.exceptions import NoCredentialsError
+from dotenv import dotenv_values
 
 class DataBase:
     def __init__(self):
         self.connection = pymysql.connect(
-            host='localhost', #ip
-            user='root',
-            password='sqlpassword',
-            db='nombreBasedeDatos'
+            host=sql_host, #ip
+            user=sqluser,
+            password=sqlpassword,
+            db=db_name
         )
 
         self.cursor = self.connection.cursor()
@@ -59,5 +63,64 @@ class DataBase:
             raise
     """
 
-database = DataBase()
-database.select_user("Varios")
+    def data_to_json(self, tabla):
+        sql = 'SELECT * FROM {}'.format(tabla)
+        sql_col = 'SHOW COLUMNS FROM {}'.format(tabla)
+
+        try:
+            print("Volcando los datos al archivo JSON...")
+            list = []
+            user_aux = {}
+            i = 0
+            self.cursor.execute(sql)
+            data_db = self.cursor.fetchall()
+            self.cursor.execute(sql_col)
+            cols = self.cursor.fetchall()
+            for user in data_db:
+                for col in cols:
+                    user_aux[col[0]] = user[i]
+                    i += 1
+                list.append(user_aux)
+                user_aux = {}
+                i = 0
+            with open(json_file, "w") as outfile:
+                json.dump(list, outfile)
+
+        except  Exception as e:
+            raise
+
+def upload_to_aws(local_file, bucket, s3_file):
+    s3 = boto3.client('s3', aws_access_key_id = ACCESS_KEY,
+                       aws_secret_access_key = SECRET_KEY)
+    
+    try:
+        s3.upload_file(local_file, bucket, s3_file)
+        print('Upload successful!')
+    except FileNotFoundError:
+        print("The file was not found")
+    except NoCredentialsError:
+        print("Credentials not available")
+
+
+if __name__ == "__main__":
+    # Get variables from config file (you should call it credentials.env)
+    config = dotenv_values('./credentials.env')
+    ACCESS_KEY = config['aws_access_key_id']
+    SECRET_KEY = config['aws_secret_access_key']
+    db_name = config['bd']
+    sqlpassword = config['sqlpassword']
+    sqluser = config['sqluser']
+    sql_host = config['sql_host']
+    table_db = config['table_db']
+    json_file = config['json_file']
+    bucket_s3 = config['bucket_s3']
+    s3_json_file = config['s3_json_file']
+
+    # Load database
+    database = DataBase()
+    database.select_user("Varios")
+    # Dump data to JSON
+    database.data_to_json(table_db)
+
+    # Upload JSON file to bucket in AWS
+    upload_to_aws(json_file, bucket_s3, s3_json_file)
